583a0e12bbdf3244f14ff2d0ca1255cf845e3b5b
gabrielibanez <g.ibanezw@gmail.com>
2018-03-16 03:06:57 +0100

    membre fini

--- app/controllers/membres_controller.rb
+++ app/controllers/membres_controller.rb
@@ -1,2 +1,48 @@
 class MembresController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_user
+
+  def index
+    @family = @user.membres
+  end
+
+  def show
+  end
+
+  def new
+    @usermembre = UserMembre.new
+    @membre = Membre.new
+  end
+
+  def create
+    @membre = Membre.new(membre_params)
+    @usermembre = UserMembre.new
+    @usermembre.user = @user
+    @usermembre.membre = @membre
+    @usermembre.save
+    if @membre.save
+      redirect_to page_membres_path(current_user)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def membre_params
+    params.require(:membre).permit(:phone_numbre, :last_name, :fisrt_name, :birthday)
+  end
 end
